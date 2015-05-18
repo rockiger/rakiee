5261718a7fcfaf9fb09b492768cfce7f7b69e837
rockiger <marco@rockiger.com>
2015-05-18 13:14:39 +0200

    do not overflow sidebar

--- src/akiee/style.clj
+++ src/akiee/style.clj
@@ -40,7 +40,7 @@
      [:#editor {:margin 0 :position "absolute" :top (px 52) :bottom 0 :left 0 :right 0}]
 
      [:#tasks {:display "flex"}]
-     [:#task-sidebar {:width "300px" :background-color "#fafafa" :border-left "1px solid #a1a1a1" :transition "width .1s linear" }]
+     [:#task-sidebar {:width "300px" :background-color "#fafafa" :border-left "1px solid #a1a1a1" :transition "width .1s linear" :overflow-x "hidden"}]
      [:#task-sidebar.closed {:width 0 :transition "width .1s linear" :overflow-x "hidden"}]
      [:#details {:padding "0 10px"}]
      [:#details [:div:hover  :h4:hover {:background-color "#f2f2f2" :cursor "pointer"}]]
